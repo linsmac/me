30f12c1ac7f9bc6562bb531a607bfaec0b9f1149
linyongsyuan <linyongsyuan@linyongsyuande-MacBook-Pro.local>
2019-07-31 23:14:51 +0800

    Initial commit

---
+++ src/main/java/com/lin/Person.java
@@ -0,0 +1,8 @@
+package com.lin;
+
+public class Person {
+    public void hello(){
+        System.out.println("hello");
+
+    }
+}
---
+++ src/main/kotlin/lin/com/Hello.kt
@@ -0,0 +1,16 @@
+package lin.com
+
+fun main(args: Array<String>) {
+    //println("Hello kotlin")
+    //Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("helllo")
+    }
+
+
+}
