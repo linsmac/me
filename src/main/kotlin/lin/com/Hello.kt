e9865b67b829ae7be77f5f55b5548f19b3088e11
linyongsyuan <herewego8520@outlook.com>
2019-07-31 23:38:35 +0800

    Java & Katlin var data type

--- src/main/kotlin/lin/com/Hello.kt
+++ src/main/kotlin/lin/com/Hello.kt
@@ -3,8 +3,16 @@ package lin.com
 fun main(args: Array<String>) {
     //println("Hello kotlin")
     //Human().hello()
-    val h=Human()
-    h.hello()
+    /*val h=Human()
+    h.hello()*/
+    //分號可有可無;
+    var age = 19;
+    age = 20;
+    var weight =66.5f;//=var weight:Float=66.5f;
+    var name:String;
+    name="Lin";
+
+
 }
 
 class Human{
